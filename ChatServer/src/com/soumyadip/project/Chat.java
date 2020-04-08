232c1e1a0301e3b68eacb74a8bbd3b9bc876593a
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-08 20:38:07 +0530

    Cord

--- ChatServer/src/com/soumyadip/project/Chat.java
+++ ChatServer/src/com/soumyadip/project/Chat.java
@@ -26,7 +26,7 @@ public class Chat extends JFrame {
 
 	public Chat() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 485, 509);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
