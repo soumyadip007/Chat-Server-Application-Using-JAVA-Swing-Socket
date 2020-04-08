48cdef00faf1380d20dd8a9ac89d2013f510b207
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-08 21:04:47 +0530

    CharClient

---
+++ ChatServer/src/com/soumyadip/project/ChatClient.java
@@ -0,0 +1,100 @@
+package com.soumyadip.project;import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class ChatClient extends JFrame {
+	private JTextField textField_1;
+	private JButton btnSend;
+	private String main="";
+	JTextArea textArea;
+//	private JScrollBar scrollBar;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChatClient frame = new ChatClient();
+					frame.setTitle("Soumyadip Project");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ChatClient() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setBounds(100, 100, 428, 657);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		textField_1 = new JTextField();
+		textField_1.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					action();
+				}
+			}
+			
+		});
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		textField_1.setBounds(0, 555, 338, 60);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				action();
+			}
+		});
+		btnSend.setBounds(341, 555, 75, 59);
+		contentPane.add(btnSend);
+		textArea = new JTextArea();
+		textArea.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		textArea.setEditable(false);
+		textArea.setBounds(0, 0, 396, 554);
+		contentPane.add(textArea);
+
+	}
+	
+	void action()
+	{
+		
+
+	}
+	
+
+	void ai(String s)
+	{
+		textArea.append(" AI->"+s+"\n\n");
+		textField_1.setText("");
+	}
+	
+}
