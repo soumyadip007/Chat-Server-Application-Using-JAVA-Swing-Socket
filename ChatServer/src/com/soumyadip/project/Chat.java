cd130767a07d9217c3ede8effab3e2dbfbd5625f
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-08 20:39:33 +0530

    Chat config

--- ChatServer/src/com/soumyadip/project/Chat.java
+++ ChatServer/src/com/soumyadip/project/Chat.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
 
 public class Chat extends JFrame {
 
@@ -24,13 +25,22 @@ public class Chat extends JFrame {
 		});
 	}
 
+	/**
+	 * Create the frame.
+	 */
 	public Chat() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 485, 509);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JTextArea textArea = new JTextArea();
+		panel.add(textArea);
 	}
 
 }
